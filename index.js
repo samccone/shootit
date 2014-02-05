ec89ce3503f016eaa3709627277dab6191c94e77
Sam Saccone <samccone@gmail.com>
2014-02-04 20:47:21 -0500

    ensure relative path to shoot script

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ var path  = require('path');
 module.exports = function(url, cb) {
   var name      = Math.random() + ".png";
   var location  = path.join("/tmp", name);
+  var script    = path.join(__dirname, 'shoot.js');
 
-  var p = spawn('phantomjs', ['shoot.js', url, location]);
+  var p = spawn('phantomjs', [script, url, location]);
+
+  p.stderr.on('data', function(e){
+    console.log("~~ AN ERROR OCCURED ~~");
+    console.log(e.toString());
+  });
 
   p.stdout.on('data', function(d) {
     console.log(d.toString());
